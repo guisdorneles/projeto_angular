43f2c9dceae7b8b5f4c64e5f3c7f23c79c277dbd
guisdorneles <guisdorneles@gmail.com>
2021-07-06 20:17:11 -0300

    expansao menu

--- projeto_angular/src/app/app.component.ts
+++ projeto_angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Menu } from './components/navegacao/models/menu';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  menu: Menu = new Menu();
+  
+  constructor(private data: DataService) {}
+  
+      ngOnInit(){      
+         this.data.currentMessage.subscribe( menu => this.menu = menu);
+      }
+  
   title = 'front-end';
 }
