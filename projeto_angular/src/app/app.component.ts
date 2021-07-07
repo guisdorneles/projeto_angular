0f59a9401720282494a6c5b492a39d15770f770a
guisdorneles <guisdorneles@gmail.com>
2021-07-06 21:31:33 -0300

    menu versao 1

--- projeto_angular/src/app/app.component.ts
+++ projeto_angular/src/app/app.component.ts
@@ -1,22 +1,11 @@
-import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { Menu } from './components/navegacao/models/menu';
-import { DataService } from './services/data.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  
-  menu: Menu = new Menu();
-  
-  constructor(private data: DataService) {}
-  
-      ngOnInit(){      
-         this.data.currentMessage.subscribe( menu => this.menu = menu);
-      }
+export class AppComponent{
+
   
   title = 'front-end';
 }
