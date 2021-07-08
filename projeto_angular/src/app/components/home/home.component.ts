e041fd287384d21502014ecc639275678700adbf
Guilherme <guisd58@hotmail.com>
2021-07-08 20:49:49 -0300

    inclusao de graficos

--- projeto_angular/src/app/components/home/home.component.ts
+++ projeto_angular/src/app/components/home/home.component.ts
@@ -1,8 +1,33 @@
 import { Component } from "@angular/core";
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
     selector: 'app-home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    public barChartOptions: ChartOptions = {
+        responsive: true,
+        // We use these empty structures as placeholders for dynamic theming.
+        scales: { xAxes: [{}], yAxes: [{}] },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end',
+          }
+        }
+      };
+      public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+      public barChartType: ChartType = 'bar';
+      public barChartLegend = true;
+      public barChartPlugins = [pluginDataLabels];
+    
+      public barChartData: ChartDataSets[] = [
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+      ];
+    
+}
